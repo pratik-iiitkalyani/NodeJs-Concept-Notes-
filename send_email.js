66fedbbc87261057df0f44f8ebdada817c0a64d4
pratik <pratik@gmail.com>
2019-10-22 19:37:37 +0530

    send email using Nodemailer

---
+++ send_email.js
@@ -0,0 +1,37 @@
+/*******Nodemailer is a module for Node.js applications
+ to allow easy as cake email sending*/
+
+// Require Nodemailer
+const Nodemailer = require('nodemailer');
+
+/*(All public Nodemailer methods support both callbacks and Promises (if callback is omitted). You need to have at least Node
+ v8.0.0 if you want to use async..await with Nodemailer.)*/
+
+ // Create Transport //
+
+ const Transporter = Nodemailer.createTransport({
+ 	service: 'gmail',		                // using gmail as transport service
+ 	auth: {
+ 		user: 'pratik@iiitkalyani.ac.in',   // specifying email ans password to login
+ 		pass: 'pk121raja@123'		// and send mail
+ 	}
+ });
+
+ //  configuration object where we will be configuring our email details.
+
+ const mailOption = {
+ 	from: 'pratik@iiitkalyani.ac.in',            // sender email
+ 	to: 'pkraja121dss@gmail.com',              // list of receiver email
+ 	subject: "Sending email via Nodemailer",   // subject of email
+ 	text: 'Hi'                                 // body of email
+ }
+
+/* Deliver the message object using the
+ sendMail() method of your previously created transporter */
+
+Transporter.sendMail(mailOption, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+});
